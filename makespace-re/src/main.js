2f3abf6dd64671a0ac8ce7f3c34173ba434b0a38
SS5hane <1005709383@qq.com>
2018-06-25 09:31:03 +0800

    初始化

---
+++ makespace-re/src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import qs from 'qs'
+Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+
+
+Vue.prototype.$axios = axios
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  created () {
+    this.$axios.defaults.baseURL='http://localhost:8080'
+  },
+  components: { App },
+  template: '<App/>'
+})
